805be09bbbeafe4f6105e76cb6880b3d13b5c257
arnab <arnab.kundu1990@gmail.com>
2021-02-03 04:35:22 +0530

    Conditional rendering

--- src/components/renderList.jsx
+++ src/components/renderList.jsx
@@ -4,14 +4,20 @@ class RenderList extends Component {
     state = {
         tags: ['tag1', 'tag2', 'tag3']
     }
+
+    // Conditional rendering
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags</p>;
+
+        //unique key needed for list item, to identify item while updating in DOM.
+        //check the warning in chrome console without key in item
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    }
+
     render() {
         return (
             <React.Fragment>
-                <ul>
-                    {/* unique key needed for list item, to identify item while updating in DOM. */}
-                    {/* check the warning in chrome console without key in item */}
-                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>
+                {this.renderTags()}
             </React.Fragment>
         );
     }

c3a7754146bb6a5609b8b58b909e5798a03f5c2b
arnab <arnab.kundu1990@gmail.com>
2021-02-02 20:11:57 +0530

    create component and render hello world

---
+++ src/components/counter.jsx
@@ -0,0 +1,13 @@
+//imrc to generate code 
+import React, { Component } from 'react';
+
+//cc to generate code
+class Counnter extends Component {
+    state = {}
+    render() {
+        return <h1>Hello world</h1>
+    }
+
+}
+
+export default Counnter;

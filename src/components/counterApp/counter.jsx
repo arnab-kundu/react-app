e2f014c1e2cb0e024cc8f7476271b5abfbee8462
arnab <arnab.kundu1990@gmail.com>
2021-02-03 18:07:45 +0530

    Removing the Local State. Delete, Reset done. Increment Todo

--- src/components/counterApp/counter.jsx
+++ src/components/counterApp/counter.jsx
@@ -1,36 +1,24 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        count: this.props.value
-    }
-
     conditionalAttribute() {
-        return this.state.count === 0 ? "badge m-2 badge-warning" : "badge m-2 badge-primary";
+        return this.props.counter.value === 0 ? "badge m-2 badge-warning" : "badge m-2 badge-primary";
     }
 
     conditionalRendering() {
-        return this.state.count === 0 ? "Zero" : this.state.count
+        return this.props.counter.value === 0 ? "Zero" : this.props.counter.value
     }
 
     render() {
-        console.log(this.props);
+        console.log(this.props.counter);
         return (
             <div>
                 {this.props.children}
                 <span className={this.conditionalAttribute()}>{this.conditionalRendering()}</span>
-                <button onClick={this.handleIncrement} className="btn btn-primary m-2" >Increment</button>
-                <button className="btn btn-danger m-2" onClick={() => this.props.onDelete(this.props.id)}>Delete</button>
+                <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-primary m-2" >Increment</button>
+                <button className="btn btn-danger m-2" onClick={() => this.props.onDelete(this.props.counter.id)}>Delete</button>
             </div>
         );
-    }
-
-    handleIncrement = () => {
-        // props cannot be edited. Its read only. To verify uncomment next line code, hit increment 
-        // and check console, will get error mentioned in 2nd next line.
-        // this.props.value = 0;
-        // Cannot assign to read only property 'value' of object
-        this.setState({ count: this.state.count + 1 })
     }
 }
 
--- src/components/counterApp/counters.jsx
+++ src/components/counterApp/counters.jsx
@@ -26,12 +26,17 @@ class Counters extends Component {
         this.setState({ counters: counters })
     }
 
+    handleIncrement = (counter) => {
+        console.log(counter);
+        //TODO
+    }
+
     render() {
         return (
             <React.Fragment>
                 <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
                 {this.state.counters.map(counter =>
-                    <Counter key={counter.id} id={counter.id} value={counter.value} onDelete={this.handleDelete} >
+                    <Counter key={counter.id} counter={counter} onIncrement={this.handleIncrement} onDelete={this.handleDelete} >
                         {/* Children prop */}
                         <h4>Counter #{counter.id}</h4>
                     </Counter>)}

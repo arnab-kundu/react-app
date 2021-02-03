8923a6232584e29d5322123d2eeb17877717951d
arnab <arnab.kundu1990@gmail.com>
2021-02-03 11:47:24 +0530

    in-line function

--- src/components/updateState.jsx
+++ src/components/updateState.jsx
@@ -16,14 +16,17 @@ class UpdateState extends Component {
     // cannot pass parament in method in onClick. 
     // onClick have only the method reference.
     // so to pass parameter in method we can do like this
+    // not using anymore
     doHandleIncrement = () => {
         this.handleIncrement({ id: 1 })
     }
+
     render() {
         return (
             <React.Fragment>
                 <span className="badge badge-primary m-2">{this.state.count}</span>
-                <button onClick={this.doHandleIncrement} className="btn btn-secondary m-2">Increment</button>
+                {/* inline  function to avoid wrapper method like doHandleIncrement */}
+                <button onClick={() => { this.handleIncrement({ id: 1 }) }} className="btn btn-secondary m-2">Increment</button>
             </React.Fragment>
         );
     }

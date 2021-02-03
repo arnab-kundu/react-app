63d17682dab9dfa34688574b8c7310590afe522f
arnab <arnab.kundu1990@gmail.com>
2021-02-03 19:28:41 +0530

    Navbar totalCount

--- src/components/counterApp/navBar.jsx
+++ src/components/counterApp/navBar.jsx
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 
 class NavBar extends Component {
     state = {}
+
     render() {
+        console.log('navbar', this.props.totalCounters)
         return (
             <nav className="navbar navbar-light bg-light">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">Navbar</a>
+                    <span className="badge badge-pill badge-secondary m-2">{this.props.totalCounters}</span>
                 </div>
             </nav>
         );
